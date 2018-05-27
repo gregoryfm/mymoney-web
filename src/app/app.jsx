7657f3998d9f9b5dd76845cb0b82522bd10ccc28
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-27 19:20:03 -0300

    initial templete

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import '../common/template/dependencies';
+import Header from '../common/template/header';
 
 export default props => (
   <div className="wrapper">
-    <h1>App</h1>
+    <Header />
   </div>
 )
