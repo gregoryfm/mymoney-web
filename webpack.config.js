da84fb106fe8cedc628164610da0433badf0c47a
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-27 16:51:53 -0300

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,17 @@ module.exports = {
     path: __dirname + '/public',
     filename: './app.js',
   },
-  webServer: {
+  devServer: {
     port: 8181,
     contentBase: './public'
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
-    alias: [
+    alias: {
       modules: __dirname + '/node_modules',
       jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
       bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
-    ]
+    }
   },
   plugins: [
     new webpack.ProvidePlugin({
