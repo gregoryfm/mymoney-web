e4e176126508a66b07028f1b2923d834db7f0c45
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-28 17:00:16 -0300

    adding redux-promise and redux dev tools

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+
+import promise from 'redux-promise';
 
 import App from './app/app';
 import reducers from './app/reducers';
 
-const store = createStore(reducers);
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = applyMiddleware(promise)(createStore)(reducers, reduxDevTools);
 
 ReactDOM.render(
   <Provider store={store}>
