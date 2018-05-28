8ba71d1d4bef523924fc6494cecad04a0bb479c9
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-27 22:05:10 -0300

    title and content components

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-export default props => (
-  <div>
-    <h1>Dashboard</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Dashboard' small='Version 0.0.1' />
+        <Content>
+          Dashboard
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
