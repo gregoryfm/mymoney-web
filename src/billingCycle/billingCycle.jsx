adabd71342d68426f10d8138cccd49fe73277b7b
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-30 21:33:58 -0300

    tabs

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
-export default props => (
-  <div>
-    <h1>Billing Cycle</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
+import Tabs from '../common/tabs/tabs';
+import TabsContent from '../common/tabs/tabsContent';
+import TabsHeader from '../common/tabs/tabsHeader';
+
+class BillingCycle extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Billing Cycle' small='Register' />
+        <Content>
+          <Tabs>
+            <TabsHeader></TabsHeader>
+            <TabsContent></TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    );
+  }
+}
+
+export default BillingCycle;
